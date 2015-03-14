e26a026e178be255ba4e55349e2de4d8fdba0ace
Pallavi G <pallavi.mp88@gmail.com>
2015-03-15 04:00:31 +0530

    handled keypad and focus issues

--- app/src/main/java/com/navpal/feedback/ui/GetstartedActivity.java
+++ app/src/main/java/com/navpal/feedback/ui/GetstartedActivity.java
@@ -3,10 +3,13 @@ package com.navpal.feedback.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.navpal.feedback.R;
 import com.navpal.feedback.util.Utils;
@@ -31,6 +34,18 @@ public class GetstartedActivity extends Activity {
         email = (AutoCompleteTextView)findViewById(R.id.email);
         password = (EditText)findViewById(R.id.password); //Not using right now
         signIn = (Button)findViewById(R.id.email_sign_in_button);
+
+        email.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    signIn.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
 
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
