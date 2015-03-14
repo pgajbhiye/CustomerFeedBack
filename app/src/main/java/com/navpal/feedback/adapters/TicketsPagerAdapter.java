297c1063b8a9dc39b38de298fba0019d0df23272
Pallavi G <pallavi.mp88@gmail.com>
2015-03-14 19:48:49 +0530

    Adding package fragments and adapter

---
+++ app/src/main/java/com/navpal/feedback/adapters/TicketsPagerAdapter.java
@@ -0,0 +1,25 @@
+package com.navpal.feedback.adapters;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Author Pallavi
+ */
+public class TicketsPagerAdapter extends FragmentStatePagerAdapter {
+
+    public TicketsPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
