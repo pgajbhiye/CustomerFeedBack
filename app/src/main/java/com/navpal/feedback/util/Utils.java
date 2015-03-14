fa02d190eb7dc3e1a596c9c46475f947f2b83dae
Pallavi G <pallavi.mp88@gmail.com>
2015-03-15 03:00:31 +0530

    added email validation code

--- app/src/main/java/com/navpal/feedback/util/Utils.java
+++ app/src/main/java/com/navpal/feedback/util/Utils.java
@@ -5,12 +5,22 @@ import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 /**
  * Author Pallavi
  */
 public class Utils {
 
+    private static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    private static Pattern pattern;
+
+    static{
+        pattern = Pattern.compile(EMAIL_PATTERN);
+    }
 
     public static void showToast(Context context,String msg){
         Toast.makeText(context,msg,Toast.LENGTH_SHORT).show();
@@ -28,6 +38,11 @@ public class Utils {
         SimpleDateFormat format = new SimpleDateFormat("dd MMM yy");
         String prettyDate = format.format(date);
         return prettyDate;
+
+    }
+
+    public static boolean isValidEmail(String emailId){
+          return pattern.matcher(emailId).matches();
 
     }
 }
